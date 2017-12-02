53576828d9fa00f2bb8f4da7766015bd7e767167
MakroCow <yoco.lie@gmail.com>
2017-12-02 12:29:52 +0100

    Added solutions to day 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { MaterializeModule } from 'angular2-materialize';
 
 
 import { AppComponent } from './app.component';
+import { Day1Component } from './day1/day1.component';
+import { Day2Component } from './day2/day2.component';
+import { Day3Component } from './day3/day3.component';
+import { Day4Component } from './day4/day4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Day1Component,
+    Day2Component,
+    Day3Component,
+    Day4Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/day3/day3.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day3',
+  templateUrl: './day3.component.html',
+  styleUrls: ['./day3.component.css']
+})
+export class Day3Component implements OnInit {
+  ergebnis : number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  antwortberechnen(){
+
+  }
+
+  antwortberechnen2(){
+
+  }
+
+
+
+}
